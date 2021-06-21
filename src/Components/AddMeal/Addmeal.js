fb82012dcdff90d348c686a82f8aed7e5c3318aa
MariamShalaby <77920984+MariamShalaby11@users.noreply.github.com>
2021-06-21 16:27:16 +0200

    ..

---
+++ src/Components/AddMeal/Addmeal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class AddMreal extends React.Component{
+    state={
+
+    }
+    render(){
+        return(
+            nbh
+        )
+    }
+}
---
+++ src/Components/Menu/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Menu extends React.Component{
+    state={
+
+    }
+    render(){
+        return(
+            nbh
+        )
+    }
+}
